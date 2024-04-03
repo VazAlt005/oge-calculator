90f6f4447152211415d1f197385ee8c0590c3cd7
Vazgen <norvardyanvazgen@gmail.com>
2024-04-03 11:44:00 +0300

    broke program

--- src/main_window.py
+++ src/main_window.py
@@ -7,7 +7,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.solve_btn.clicked.connect(self.solve)  # 1 исправление
+
 
     def solve(self):
         base1 = self.ui.base1_label.text()
@@ -17,13 +17,10 @@ class MainWindow(QMainWindow):
             not base1
             or not base2
             or not height
-            or not base1.isdigit()  # 2 исправление
-            or not base2.isdigit()  # 2 исправление
-            or not height.isdigit()  # 2 исправление
         ):
             QMessageBox.warning(self, "Ошибка", "Некорректные данные")
             return
         answer = ((int(base1) + int(base2)) / 2) * int(height)
         QMessageBox.information(
-            self, "Решение", "Площадь черерехугольника = " + str(answer) # 3 исправление
+            self, "Решение", str(answer)
         )  
